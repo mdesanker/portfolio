0c2d695c90b85efd663ed4695bbd2f137150872b
Michael Desanker <mdesanker@gmail.com>
2022-02-04 09:20:57 -0500

    Content text added to home component

--- src/components/views/home/Home.tsx
+++ src/components/views/home/Home.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 import Background from "../../../assets/images/home-background.jpg";
 
@@ -5,7 +6,19 @@ const Home = () => {
   return (
     <Wrapper>
       <Card>
-        <h1>michael desanker</h1>
+        <Title>michael desanker</Title>
+        <Text>
+          I am a self-taught software engineer with a background in chemistry.
+          Learn more <StyledLink to="#">about</StyledLink> me.
+        </Text>
+        <Text>
+          Here you can see some of my latest{" "}
+          <StyledLink to="#">projects.</StyledLink>
+        </Text>
+        <Text>
+          For questions or hellos, feel free to{" "}
+          <StyledLink to="#">contact me.</StyledLink>
+        </Text>
       </Card>
     </Wrapper>
   );
@@ -35,6 +48,26 @@ const Card = styled.div`
   @media only screen and (max-width: 500px) {
     width: 100%;
   }
+`;
+
+const Title = styled.h1`
+  letter-spacing: 0.2rem;
+  margin-bottom: 1rem;
+`;
+
+const Text = styled.p`
+  width: 100%;
+  font-weight: bold;
+  letter-spacing: 0.1rem;
+  padding-top: 1rem;
+`;
+
+const StyledLink = styled(Link)`
+  text-transform: uppercase;
+  text-decoration: none;
+  font-weight: bold;
+  letter-spacing: 0.1rem;
+  color: #b5ebe4;
 `;
 
 export default Home;
