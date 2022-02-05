04bf81d4e51456ea695409244640ff622b667b58
Michael Desanker <mdesanker@gmail.com>
2022-02-05 16:58:17 -0500

    Clean unused code in about component

--- src/components/views/about/About.tsx
+++ src/components/views/about/About.tsx
@@ -17,37 +17,10 @@ const About = () => {
 };
 
 const Wrapper = styled.main`
-  padding-top: 160px;
+  padding-top: 120px;
   display: flex;
   flex-direction: column;
   align-items: center;
-`;
-
-const Section = styled.section`
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-  width: 70%;
-  max-width: 800px;
-`;
-
-const Title = styled.h1`
-  text-transform: uppercase;
-`;
-
-const Text = styled.p`
-  text-align: justify;
-`;
-
-const SkillSection = styled.div`
-  display: flex;
-  flex-direction: column;
-  padding-bottom: 1rem;
-`;
-
-const SkillHeader = styled.p`
-  font-weight: bold;
-  padding-bottom: 1rem;
 `;
 
 export default About;
