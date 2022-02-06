9ef69a24f0b3eb6d143a6f7da4fbf6ef4012e4ee
Michael Desanker <mdesanker@gmail.com>
2022-02-05 19:58:14 -0500

    Add basic content to contact component

--- src/components/views/contact/Contact.tsx
+++ src/components/views/contact/Contact.tsx
@@ -9,9 +9,45 @@ const Contact = () => {
       <Nav />
       <Wrapper>
         <h1>Contact</h1>
+
+        <LinkContainer>
+          <Email>&#47;&#47; Email</Email>
+          <StyledLink
+            href="https://www.linkedin.com/in/michaeldesanker/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            &#47;&#47; LinkedIn
+          </StyledLink>
+          <StyledLink
+            href="https://github.com/mdesanker"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            &#47;&#47; GitHub
+          </StyledLink>
+        </LinkContainer>
       </Wrapper>
     </Fragment>
   );
 };
+
+const LinkContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
+  padding-top: 2rem;
+`;
+
+const Email = styled.p`
+  font-size: 1.5rem;
+`;
+
+const StyledLink = styled.a`
+  font-size: 1.5rem;
+  color: black;
+  text-decoration: none;
+`;
 
 export default Contact;
