74b924fa0314d3d838d7020be01ce554c51396ca
Michael Desanker <mdesanker@gmail.com>
2022-02-05 20:11:22 -0500

    Add hover effects

--- src/components/views/contact/Contact.tsx
+++ src/components/views/contact/Contact.tsx
@@ -1,9 +1,19 @@
 import styled from "styled-components";
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import Nav from "../../elements/nav/Nav";
 import Wrapper from "../../elements/Wrapper";
 
 const Contact = () => {
+  const [email, setEmail] = useState("// mdesanker@gmail.com");
+
+  const mouseOverHandler = () => {
+    setEmail("Copy to clipboard");
+  };
+
+  const mouseOutHandler = () => {
+    setEmail("// mdesanker@gmail.com");
+  };
+
   return (
     <Fragment>
       <Nav />
@@ -11,20 +21,22 @@ const Contact = () => {
         <h1>Contact</h1>
 
         <LinkContainer>
-          <Email>&#47;&#47; Email</Email>
-          <StyledLink
-            href="https://www.linkedin.com/in/michaeldesanker/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            &#47;&#47; LinkedIn
-          </StyledLink>
+          <Email onMouseOver={mouseOverHandler} onMouseOut={mouseOutHandler}>
+            {email}
+          </Email>
           <StyledLink
             href="https://github.com/mdesanker"
             target="_blank"
             rel="noopener noreferrer"
           >
             &#47;&#47; GitHub
+          </StyledLink>
+          <StyledLink
+            href="https://www.linkedin.com/in/michaeldesanker/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            &#47;&#47; LinkedIn
           </StyledLink>
         </LinkContainer>
       </Wrapper>
@@ -36,18 +48,29 @@ const LinkContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  gap: 20px;
+  gap: 40px;
   padding-top: 2rem;
 `;
 
 const Email = styled.p`
   font-size: 1.5rem;
+  cursor: pointer;
+  // transition: color 100ms ease;
+
+  &:hover {
+    color: gray;
+  }
 `;
 
 const StyledLink = styled.a`
   font-size: 1.5rem;
   color: black;
   text-decoration: none;
+  // transition: color 100ms ease;
+
+  &:hover {
+    color: gray;
+  }
 `;
 
 export default Contact;
