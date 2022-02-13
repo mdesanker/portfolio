93390e4990e743f4f21c51c24280c67eede5f1d8
Michael Desanker <mdesanker@gmail.com>
2022-02-12 21:22:59 -0500

    Add scroll to top wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,20 @@ import Home from "./components/views/home/Home";
 import About from "./components/views/about/About";
 import Contact from "./components/views/contact/Contact";
 import Projects from "./components/views/projects/Projects";
+import ScrollToTop from "./components/elements/ScrollToTop";
 
 function App() {
   return (
     <Fragment>
       <GlobalStyles />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
+      <ScrollToTop>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </ScrollToTop>
     </Fragment>
   );
 }
