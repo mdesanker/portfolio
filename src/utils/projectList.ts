18d7b529b406f0234b89c76a93d14ffa0611abd2
Michael Desanker <mdesanker@gmail.com>
2022-03-05 08:59:04 -0500

    Create module with list of projects

---
+++ src/utils/projectList.ts
@@ -0,0 +1,18 @@
+export const projectList = [
+  {
+    id: 1,
+    title: "Budget App",
+    image: "/budget_app.png",
+    tools: ["TypeScript", "React", "Redux", "NodeJS", "Express", "MongoDB"],
+    description:
+      "This app allows users to track expenses and earnings, and view spending trends in week, year, and category charts.",
+  },
+  {
+    id: 2,
+    title: "Errand Tracker App",
+    image: "errand_app.png",
+    tools: ["React", "Redux", "NodeJS", "Express", "MongoDB"],
+    description:
+      "This errand tracking app allows users to create errands with titles, due dates, and priorities. Errand completion status is toggled by clicking the errand. Errands can be added to projects, which can be shared with and updated by friends.",
+  },
+];
